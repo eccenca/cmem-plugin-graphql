45df11d4efe7b34ba2b65868b03d8096db547d7a
Sai Praneeth M <saipraneeth@aarth.io>
2022-05-19 12:30:14 +0530

    Use gql api to invoke GraphQL endpoints

--- cmem_plugin_graphql/__init__.py
+++ cmem_plugin_graphql/__init__.py
@@ -1,29 +1,34 @@
-"""Random values workflow plugin module"""
+"""GraphQL workflow plugin module"""
 import uuid
-from secrets import token_urlsafe
+from typing import List
 
-from cmem_plugin_base.dataintegration.description import Plugin, PluginParameter
-from cmem_plugin_base.dataintegration.entity import (
-    Entities, Entity, EntitySchema, EntityPath,
-)
-from cmem_plugin_base.dataintegration.plugins import WorkflowPlugin
-from cmem_plugin_base.dataintegration.types import StringParameterType
 import validators
-
+from cmem.cmempy.workspace.projects.resources.resource import create_resource
+from cmem_plugin_base.dataintegration.description import Plugin, PluginParameter
+from cmem_plugin_base.dataintegration.entity import Entity, Entities, EntityPath, EntitySchema
 from cmem_plugin_base.dataintegration.parameter.dataset import DatasetParameterType
+from cmem_plugin_base.dataintegration.plugins import WorkflowPlugin
+from cmem_plugin_base.dataintegration.utils import setup_cmempy_super_user_access
+import io
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+import json
+from cmem.cmempy.workspace.tasks import get_task
 
 
 @Plugin(
-    label="GraphQL (awesome)",
+    label="GraphQL ",
     description="Retrieves the data from GraphQL APIs",
     documentation="""
-Executes a GraphQL APIs based on fixed configuration and/or input parameters and returns the result as entity.
+Executes a GraphQL APIs based on fixed configuration and/or
+input parameters and returns the result as entity.
 """,
     parameters=[
         PluginParameter(
             name="graphql_url",
             label="URL",
-            description="The URL to execute this request against. This can be overwritten at execution time via input."
+            description="""The URL to execute this request against.
+            This can be overwritten at execution time via input."""
         ),
         PluginParameter(
             name="graphql_query",
@@ -39,8 +44,8 @@ Executes a GraphQL APIs based on fixed configuration and/or input parameters and
         )
     ]
 )
-class DollyPlugin(WorkflowPlugin):
-    """Example Workflow Plugin: Random Values"""
+class GraphQLPlugin(WorkflowPlugin):
+    """GraphQL Workflow Plugin to query GraphQL APIs"""
 
     def __init__(
             self,
@@ -56,6 +61,32 @@ class DollyPlugin(WorkflowPlugin):
         self.graphql_query = graphql_query
         self.graphql_dataset = graphql_dataset
 
-    def execute(self, inputs=()) -> None:
-        self.log.info("Start querying APIs")
+    def execute(self, inputs=()):
+        self.log.info("Start GraphQL query.")
+        # self.log.info(f"Config length: {len(self.config.get())}")
 
+        # Select your transport with a defined url endpoint
+        transport = AIOHTTPTransport(url=self.graphql_url)
+
+        # Create a GraphQL client using the defined transport
+        client = Client(transport=transport, fetch_schema_from_transport=True)
+
+        # Execute the query on the transport
+        result = client.execute(gql(self.graphql_query))
+
+        self._write_response_to_resource(result)
+
+    def _write_response_to_resource(self, response) -> None:
+        setup_cmempy_super_user_access()
+        project_resource = self.graphql_dataset.split(":")
+        file_path = f'dummy.txt'
+        file = open(file_path, "w+")
+        file.write(json.dumps(response))
+        file.close()
+        with open(file_path, 'rb') as response_file:
+            create_resource(
+                project_name=project_resource[0],
+                resource_name=project_resource[1],
+                file_resource=response_file,
+                replace=True,
+            )
--- tests/test_graphql.py
+++ tests/test_graphql.py
@@ -1,14 +1,25 @@
 """Plugin tests."""
-from cmem_plugin_graphql import DollyPlugin
+from cmem_plugin_graphql import GraphQLPlugin
 
 
 def test_execution():
     """Test plugin execution"""
-    entities = 100
-    values = 10
-
-    plugin = DollyPlugin(number_of_entities=entities, number_of_values=values)
-    result = plugin.execute()
-    for item in result.entities:
-        assert len(item.values) == len(result.schema.paths)
-
+    pass
+    url = "https://fruits-api.netlify.app/graphql"
+    query = """
+    query allFruits {
+  fruits {
+    id,
+    scientific_name,
+    fruit_name,
+    description,
+    producing_countries {
+      country
+    }
+  }
+}
+"""
+    dataset = "TestPlugins_d6f3594e5c9c450d:EmptyJSON_87bd1749821c429b"
+    plugin = GraphQLPlugin(graphql_url=url, graphql_query=query, graphql_dataset=dataset)
+    # TODO
+    # result = plugin.execute()

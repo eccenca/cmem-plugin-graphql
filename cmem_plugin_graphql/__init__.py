550c1f6c7acbd4cd6729455b0ccc1458527eaca4
Sai Praneeth M <saipraneeth@aarth.io>
2022-05-20 14:36:17 +0530

    Validation check for query structure

--- cmem_plugin_graphql/__init__.py
+++ cmem_plugin_graphql/__init__.py
@@ -12,6 +12,7 @@ from cmem_plugin_base.dataintegration.plugins import WorkflowPlugin
 from cmem_plugin_base.dataintegration.utils import setup_cmempy_super_user_access
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
+from graphql import GraphQLSyntaxError
 
 
 @Plugin(
@@ -32,7 +33,6 @@ input parameters and returns the result as entity.
             name="graphql_query",
             label="Query",
             description="GraphQL Query",
-
         ),
         PluginParameter(
             name="graphql_dataset",
@@ -52,17 +52,18 @@ class GraphQLPlugin(WorkflowPlugin):
             graphql_dataset: str = ""
             # Instead of None we are using SPACE to make mypy happy with line 68
     ) -> None:
-        """
 
-        Parameters
-        ----------
-        graphql_dataset : object
-        """
         self.graphql_url = graphql_url
         if not validators.url(graphql_url):
             raise ValueError(
                 "Provide a valid GraphQL URL."
             )
+
+        if not self._is_query_valid(graphql_query):
+            raise ValueError(
+                "Query string is not Valid"
+            )
+
         self.graphql_query = graphql_query
         self.graphql_dataset = graphql_dataset
 
@@ -84,6 +85,13 @@ class GraphQLPlugin(WorkflowPlugin):
         result = client.execute(gql(self.graphql_query))
 
         self._write_response_to_resource(result)
+
+    def _is_query_valid(self, query) -> bool:
+        try:
+            gql(query)
+            return True
+        except GraphQLSyntaxError:
+            return False
 
     def _get_resource_name(self) -> str:
         """Get resource name for selected dataset"""

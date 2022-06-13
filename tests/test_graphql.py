ebf363118d5c92a73270b05b030cb43241aa15d2
Sebastian Tramp <sebastian.tramp@eccenca.com>
2022-06-13 15:04:34 +0200

    format with black

--- tests/test_graphql.py
+++ tests/test_graphql.py
@@ -9,7 +9,7 @@ from cmem.cmempy.workspace.projects.resources.resource import get_resource_respo
 from cmem_plugin_graphql import GraphQLPlugin
 from .utils import needs_cmem
 
-GRAPHQL_URL = 'https://fruits-api.netlify.app/graphql'
+GRAPHQL_URL = "https://fruits-api.netlify.app/graphql"
 
 PROJECT_NAME = "graphql_test_project"
 DATASET_NAME = "sample_fruit"
@@ -20,12 +20,13 @@ DATESET_ID = f"{PROJECT_NAME}:{DATASET_NAME}"
 @pytest.fixture(scope="module")
 def setup(request):
     make_new_project(PROJECT_NAME)
-    make_new_dataset(project_name=PROJECT_NAME,
-                     dataset_name=DATASET_NAME,
-                     dataset_type="json",
-                     parameters={"file": RESOURCE_NAME},
-                     autoconfigure=False
-                     )
+    make_new_dataset(
+        project_name=PROJECT_NAME,
+        dataset_name=DATASET_NAME,
+        dataset_type="json",
+        parameters={"file": RESOURCE_NAME},
+        autoconfigure=False,
+    )
 
     def teardown():
         delete_project(PROJECT_NAME)
@@ -36,10 +37,12 @@ def setup(request):
 @needs_cmem
 def test_execution(setup):
     """Test plugin execution"""
-    query = 'query{fruit(id:1){id,fruit_name}}'
+    query = "query{fruit(id:1){id,fruit_name}}"
     graphql_response = "{'fruit': {'id': '1', 'fruit_name': 'Manzana'}}"
 
-    plugin: GraphQLPlugin = GraphQLPlugin(graphql_url=GRAPHQL_URL, graphql_query=query, graphql_dataset=DATESET_ID)
+    plugin: GraphQLPlugin = GraphQLPlugin(
+        graphql_url=GRAPHQL_URL, graphql_query=query, graphql_dataset=DATESET_ID
+    )
     plugin.execute()
     with get_resource_response(PROJECT_NAME, RESOURCE_NAME) as response:
         print(response.json())
@@ -47,20 +50,28 @@ def test_execution(setup):
 
 
 def test_validate_invalid_inputs():
-    query = 'query{fruit(id:1){id,fruit_name}}'
+    query = "query{fruit(id:1){id,fruit_name}}"
 
     # Invalid Query
-    invalid_query = 'query1{fruit(id:1){id,fruit_name}}'
-    invalid_url = 'fruits_invalid'
+    invalid_query = "query1{fruit(id:1){id,fruit_name}}"
+    invalid_url = "fruits_invalid"
 
     # Invalid URL
     with pytest.raises(ValueError, match="Provide a valid GraphQL URL."):
-        GraphQLPlugin(graphql_url=invalid_url, graphql_query=query, graphql_dataset=DATESET_ID)
+        GraphQLPlugin(
+            graphql_url=invalid_url, graphql_query=query, graphql_dataset=DATESET_ID
+        )
 
     # Invalid query
     with pytest.raises(ValueError, match="Query string is not Valid"):
-        GraphQLPlugin(graphql_url=GRAPHQL_URL, graphql_query=invalid_query, graphql_dataset=DATESET_ID)
+        GraphQLPlugin(
+            graphql_url=GRAPHQL_URL,
+            graphql_query=invalid_query,
+            graphql_dataset=DATESET_ID,
+        )
 
     # Invalid Dateset
     with pytest.raises(ValueError, match="None is not a valid task ID."):
-        GraphQLPlugin(graphql_url=GRAPHQL_URL, graphql_query=query, graphql_dataset="None")
+        GraphQLPlugin(
+            graphql_url=GRAPHQL_URL, graphql_query=query, graphql_dataset="None"
+        )
